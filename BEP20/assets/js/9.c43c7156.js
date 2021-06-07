b149ea4a41ee21dca447de09dd716acd29222bbd
Stackbit <projects@stackbit.com>
2021-06-07 13:04:27 +0000

    initial commit by Stackbit

---
+++ BEP20/assets/js/9.c43c7156.js
@@ -0,0 +1,187 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+    [9],
+    {
+        336: function (t, e, a) {
+            "use strict";
+            a.r(e);
+            var s = { name: "Home" },
+                i = a(40),
+                o = Object(i.a)(
+                    s,
+                    function () {
+                        var t = this,
+                            e = t.$createElement,
+                            a = t._self._c || e;
+                        return a(
+                            "div",
+                            [
+                                a(
+                                    "b-jumbotron",
+                                    {
+                                        staticClass: "purple-gradient mx-0",
+                                        attrs: {
+                                            "text-variant": "white",
+                                            header: "Kazifier - BEP20 Token Creator",
+                                            fluid: "",
+                                        },
+                                        scopedSlots: t._u([
+                                            {
+                                                key: "lead",
+                                                fn: function () {
+                                                    return [
+                                                        t._v(
+                                                            "\n            Create a BEP20 Token with in minutes with the new Smart Contract Creator for BEP20 Token on Binance Smart Chain with multiple options.\n            "
+                                                        ),
+                                                        a("br"),
+                                                        t._v(
+                                                            "\n            No login. No setup. No coding required.\n        "
+                                                        ),
+                                                    ];
+                                                },
+                                                proxy: !0,
+                                            },
+                                        ]),
+                                    },
+                                    [
+                                        t._v(" "),
+                                        a("hr", { staticClass: "my-5" }),
+                                        t._v(" "),
+                                        a(
+                                            "p",
+                                            [
+                                                a(
+                                                    "b-button",
+                                                    {
+                                                        staticClass: "my-2 py-3 px-5 text-uppercase",
+                                                        attrs: {
+                                                            to: "/create-token/",
+                                                            size: "lg",
+                                                            variant: "success",
+                                                        },
+                                                    },
+                                                    [
+                                                        t._v(
+                                                            "\n                Create BEP20 Token\n            "
+                                                        ),
+                                                    ]
+                                                ),
+                                                t._v(" "),
+                                                a(
+                                                    "a",
+                                                    {
+                                                        directives: [
+                                                            {
+                                                                name: "smooth-scroll",
+                                                                rawName: "v-smooth-scroll",
+                                                                value: {
+                                                                    duration: 1e3,
+                                                                    offset: 0,
+                                                                    updateHistory: !1,
+                                                                },
+                                                                expression:
+                                                                    "{ duration: 1000, offset: 0, updateHistory: false }",
+                                                            },
+                                                        ],
+                                                        staticClass:
+                                                            "btn btn-lg btn-outline-light py-3 px-5 text-uppercase",
+                                                        attrs: { href: "#pricing" },
+                                                    },
+                                                    [t._v("\n                Pricing\n            ")]
+                                                ),
+                                            ],
+                                        ),
+                                    ]
+                                ),
+                                t._v(" "),
+                                a(
+                                    "b-row",
+                                    {
+                                        staticClass: "aqua-gradient mx-0",
+                                        attrs: { id: "pricing" },
+                                    },
+                                    [
+                                        a(
+                                            "b-col",
+                                            {
+                                                staticClass: "py-5",
+                                                attrs: { lg: "12", xl: "10", "offset-xl": "1" },
+                                            },
+                                            [
+                                                a(
+                                                    "h2",
+                                                    {
+                                                        staticClass:
+                                                            "p-5 font-weight-lighter text-center text-light",
+                                                    },
+                                                    [t._v("Pricing")]
+                                                ),
+                                                t._v(" "),
+                                                a("ui--pricing-table"),
+                                            ],
+                                        ),
+                                    ],
+                                ),
+                                t._v(" "),
+                                a(
+                                    "b-row",
+                                    { staticClass: "purple-gradient text-white mx-0" },
+                                    [
+                                        a(
+                                            "b-col",
+                                            {
+                                                staticClass: "mb-3 text-center",
+                                                attrs: { lg: "10", "offset-lg": "1" },
+                                            },
+                                            [
+                                                a(
+                                                    "h2",
+                                                    {
+                                                        staticClass:
+                                                            "pt-5 mb-4 font-weight-lighter text-light",
+                                                    },
+                                                    [t._v("Ready to deploy your BEP20 Token?")]
+                                                ),
+                                                t._v(" "),
+                                                a("p", { staticClass: "font-weight-light" }, [
+                                                    t._v(
+                                                        "\n                Try building your BEP20 Token in less than a minute. You can try on Test Network before to go live.\n            "
+                                                    ),
+                                                ]),
+                                                t._v(" "),
+                                                a(
+                                                    "b-button",
+                                                    {
+                                                        staticClass: "my-5 py-3 px-5 text-uppercase",
+                                                        attrs: {
+                                                            to: "/create-token/",
+                                                            size: "lg",
+                                                            variant: "success",
+                                                        },
+                                                    },
+                                                    [
+                                                        t._v(
+                                                            "\n                Create BEP20 Token\n            "
+                                                        ),
+                                                    ]
+                                                ),
+                                            ],
+                                            1
+                                        ),
+                                    ],
+                                    1
+                                ),
+                                t._v(" "),
+                            ],
+                            1
+                        );
+                    },
+                    [],
+                    !1,
+                    null,
+                    null,
+                    null
+                );
+            e.default = o.exports;
+        },
+    },
+]);
